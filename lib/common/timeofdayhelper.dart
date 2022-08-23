e67a93b70610c73fcdd4b20ae462634f1c034bc6
Lance J <lancear153@gmail.com>
2022-08-23 08:05:44 +0200

    nice looking slep
    Co-authored-by: Tomassetti <scarcheek@users.noreply.github.com>

--- lib/common/timeofdayhelper.dart
+++ lib/common/timeofdayhelper.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:timezone/timezone.dart';
-
 import 'package:timezone/timezone.dart' as tz;
 
 TZDateTime tzTimeOfToday(TimeOfDay timeOfDay) {
@@ -13,4 +12,12 @@ TZDateTime tzTimeOfToday(TimeOfDay timeOfDay) {
 DateTime timeOfToday(TimeOfDay timeOfDay) {
   return DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day,
       timeOfDay.hour, timeOfDay.minute, DateTime.now().second);
+}
+
+String dayTimeFormat(TimeOfDay dateTime) {
+  return "${dateTime.hour < 10 ? "0${dateTime.hour}" : dateTime.hour}:${dateTime.minute < 10 ? "0${dateTime.minute}" : dateTime.minute}";
+}
+
+String durationHourMinFormat(int durationInMins) {
+  return "${((durationInMins ~/ 60) > 0) ? "${durationInMins ~/ 60} h" : ""} ${(durationInMins % 60 > 0) ? "${durationInMins % 60} min" : ""}";
 }
