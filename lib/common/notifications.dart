6edd8cf1cda4faaf77297e6b60b8c4313f01fb83
Tomassetti <43643522+scarcheek@users.noreply.github.com>
2022-09-01 12:56:31 +0200

    Notification icon

--- lib/common/notifications.dart
+++ lib/common/notifications.dart
@@ -12,7 +12,7 @@ enum NotificationChannel { instant, scheduled }
 initializeNotifications() async {
   // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
   const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('app_icon');
+      AndroidInitializationSettings('notification_icon');
   const IOSInitializationSettings initializationSettingsIOS =
       IOSInitializationSettings();
 
